fabf9861aa74b733e32428b897b3d25d20b5ea09
Jonatan Machado <jonatanmachado35@gmail.com>
2025-11-13 01:41:40 -0300

    first commit

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { LocationService } from '../location/location.service';
 import { jwtModuleConfig } from '../config/jwt.config';
+import { CacheService } from '../cache/cache.service';
 
 @Module({
   imports: [
@@ -14,6 +15,6 @@ import { jwtModuleConfig } from '../config/jwt.config';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocationService],
+  providers: [AuthService, LocationService, CacheService],
 })
 export class AuthModule {}
